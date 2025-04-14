dd36bcb2844a765c0b0446ca1d61a17d64b351cf
Raul Lize Teixeira <114193996+Raullize@users.noreply.github.com>
2025-04-14 18:14:55 -0300

    🧹 cleanup: formata arquivos com ESLint para manter padrão de código

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        "octo-blue": "#152D35",
-        "octo-green": "#03493D",
-        "octo-yellow": "#F9F9E0",
+        'octo-blue': '#152D35',
+        'octo-green': '#03493D',
+        'octo-yellow': '#F9F9E0'
       },
       fontFamily: {
-        bebas: ['"Bebas Neue"', "sans-serif"],
-        space: ['"Space Grotesk"', "sans-serif"],
-        archivo: ['"Archivo Black"', "sans-serif"],
-        inter: ["Inter", "sans-serif"],
-        roboto: ["Roboto", "sans-serif"],
-        opensans: ['"Open Sans"', "sans-serif"],
-      },
-    },
+        bebas: ['"Bebas Neue"', 'sans-serif'],
+        space: ['"Space Grotesk"', 'sans-serif'],
+        archivo: ['"Archivo Black"', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+        opensans: ['"Open Sans"', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
